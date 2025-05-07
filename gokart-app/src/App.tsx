3094cb9cfed905a0e4ab131687f3c97b0aafef7b
Mahtte94 <mahtias.jebrand@live.se>
2025-05-07 16:54:14 +0200

    made controllable rectangle

--- gokart-app/src/App.tsx
+++ gokart-app/src/App.tsx
@@ -1,21 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Gokart from './components/gokart'
+
 
 function App() {
 
 
   return (
     <>
-    
-      <h1 className='text-amber-300'>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Gokart/>
     </>
   )
 }
